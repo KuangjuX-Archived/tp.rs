892ed92dc1314f5a43f1a2c5eb7267bb915d3bfd
KuangjuX <qcx@tju.edu.cn>
2021-09-08 08:47:47 -0700

    init

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,23 +1,27 @@
 use std::sync::mpsc::*;
 use std::sync::Mutex;
-use std::thread::JoinHandle;
+use std::thread::*;
 
 enum ThreadPoolMessage {
     RunJon(Box<dyn FnOnce() + Send + 'static>),
     Shutdown
 }
 
+type Task = Box<dyn FnOnce() + Send + 'static>;
+
 pub struct SharedQueueThreadPool {
     receiver: Mutex<Receiver<ThreadPoolMessage>>,
-    size: usize
+    pool: Vec<JoinHandle<Task>>,
+    capacity: usize
 }
 
 impl SharedQueueThreadPool {
-    pub fn new(size: usize) -> Self {
+    pub fn new(capacity: usize) -> Self {
         let (sender, receiver) = channel::<ThreadPoolMessage>();
         Self {
             receiver: Mutex::new(receiver),
-            size
+            pool: vec![],
+            capacity
         }
     }
 }
